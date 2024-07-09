2d23676bfe78fab33e2896202f4b498d5b07c046
Jivin Varghese <jivinvarghese007@gmail.com>
2024-07-09 09:59:54 -0400

    userDetail Model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserDetail(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    interests = models.TextField(blank=True)
+
+
