46c7f686aa9618ff3835481c5637f4bc95f7e795
HappyMehta <happymehta2001@gmail.com>
2024-07-14 17:37:30 -0400

    create post html page added

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,14 +2,28 @@
 from django import forms
 from .models import Post
 
+# class PostForm(forms.ModelForm):
+#     tags = forms.CharField(max_length=200, required=False, help_text='Enter tags separated by commas.')
+#
+#     class Meta:
+#         model = Post
+#         fields = ['post_title', 'image', 'content']
+#         widgets = {
+#             'post_title': forms.TextInput(attrs={'class': 'form-control'}),
+#             'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+#             'content': forms.Textarea(attrs={'class': 'form-control'}),
+#         }
+#
+
 class PostForm(forms.ModelForm):
     tags = forms.CharField(max_length=200, required=False, help_text='Enter tags separated by commas.')
 
     class Meta:
         model = Post
-        fields = ['post_title', 'image', 'content']
+        fields = ['post_title', 'image', 'content', 'tags']
         widgets = {
-            'post_title': forms.TextInput(attrs={'class': 'form-control'}),
-            'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'post_title': forms.TextInput(attrs={'class': 'title-input', 'placeholder': 'Title'}),
+            'content': forms.Textarea(attrs={'class': 'content-textarea', 'placeholder': 'Tell your story...'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'image-input'}),
+            'tags': forms.TextInput(attrs={'class': 'tags-input', 'placeholder': 'Add tags...'}),
         }
