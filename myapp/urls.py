89fc32e78570b981c87682c37beffbc9ebc93886
Jivin Varghese <jivinvarghese007@gmail.com>
2024-07-13 21:06:17 -0400

    comments section ui

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,10 @@
 # botanic/urls.py
-from django.urls import path
+from django.urls import path, include
+
+from . import views
 from .views import base
 
 urlpatterns = [
     path('', base, name='base'),
+    path('comments/', views.comments, name='comments'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def base(request):
     return render(request, 'base.html')
+
+
+def comments(request):
+    return render(request, 'comment.html')
